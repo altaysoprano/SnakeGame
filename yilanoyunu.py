ca80439b8e8c000f155f1dde8e78a552ba28d7b7
Hasan Cihad Altay <caltayzzz@gmail.com>
2021-12-30 01:43:17 +0300

    grafikler geliştirildi

--- yilanoyunu.py
+++ yilanoyunu.py
@@ -6,21 +6,28 @@ hiz = 0.15
 
 pencere = turtle.Screen()
 pencere.title("Yılan Oyunu")
-pencere.bgcolor('lightgreen')
+pencere.bgpic('toprak.gif')
 pencere.setup(width=600, height=600)
 pencere.tracer(0) 
 
+turtle.register_shape('yilankafayukari.gif')
+turtle.register_shape('yilankafasol.gif')
+turtle.register_shape('yilankafasag.gif')
+turtle.register_shape('yilankafaasagi.gif')
+turtle.register_shape('elma.gif')
+
 kafa = turtle.Turtle()
+kafa.shape('yilankafaasagi.gif')
 kafa.speed(0)
-kafa.shape('square')
-kafa.color('black')
+kafa.shapesize(1.0, 1.0, 1)
+kafa.setheading(90)
 kafa.penup()
 kafa.goto(0,100)
 kafa.direction = 'stop'
 
 yemek = turtle.Turtle()
 yemek.speed(0)
-yemek.shape('circle')
+yemek.shape('elma.gif')
 yemek.color('red')
 yemek.penup()
 yemek.goto(0, 0)
@@ -56,18 +63,22 @@ def move():
 def goUp():
     if kafa.direction != 'down':
         kafa.direction = 'up'
+        kafa.shape('yilankafayukari.gif')
 
 def goDown():
     if kafa.direction != 'up':
         kafa.direction = 'down'
+        kafa.shape('yilankafaasagi.gif')
 
 def goRight():
     if kafa.direction != 'left':
         kafa.direction = 'right'
+        kafa.shape('yilankafasag.gif')
 
 def goLeft():
     if kafa.direction != 'right':
         kafa.direction = 'left'
+        kafa.shape('yilankafasol.gif')
 
 pencere.listen()
 pencere.onkey(goUp, 'Up')
@@ -88,22 +99,24 @@ while True:
 
         kuyruklar = []
         puan = 0
+        hiz = 0.15
         yaz.clear()
         yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
 
     if kafa.distance(yemek) < 20:
-        x = random.randint(-250, 250)
-        y = random.randint(-250, 250)
+        x = random.randint(0, 14)*20
+        y = random.randint(0, 14)*20
         yemek.goto(x, y)
 
         puan = puan + 10
+        hiz = hiz - 0.003
         yaz.clear()
         yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
 
         yeniKuyruk = turtle.Turtle()
         yeniKuyruk.speed(0)
-        yeniKuyruk.shape('square')
-        yeniKuyruk.color('white')
+        yeniKuyruk.shape('circle')
+        yeniKuyruk.color('green')
         yeniKuyruk.penup()
         kuyruklar.append(yeniKuyruk)
 
@@ -130,6 +143,7 @@ while True:
             
             kuyruklar = []
             puan = 0
+            hiz = 0.15
             yaz.clear()
             yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
 
